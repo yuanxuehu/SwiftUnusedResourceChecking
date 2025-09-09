257d2f80a0d1bd99a834725796ce4c69aa3575e6
TigerHu <xuehu.yuan@kumai-inc.com>
2025-09-09 12:25:28 +0800

    Initial Commit

---
+++ SwiftUnusedResourceChecking/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SwiftUnusedResourceChecking
+//
+//  Created by TigerHu on 2025/9/9.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
